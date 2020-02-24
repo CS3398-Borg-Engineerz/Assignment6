e0e869d1f39a0bba456413bda7eb6aada35a0f5d
JArmstrong <yak3@txstate.edu>
2020-02-23 20:45:56 -0600

    Removed the worker and SuperWorker classes from main
    I forgot to delete these in last push. Now they are deleted

--- threesolid/ThreeSolidMain.java
+++ threesolid/ThreeSolidMain.java
@@ -42,22 +42,3 @@ interface IWorker {
 	public void eat();
 }   
 
-class Worker implements IWorker{
-	public void work() {
-		// ....working
-	}
-
-	public void eat() {
-		//.... eating in launch break
-	}
-}
-
-class SuperWorker implements IWorker{
-	public void work() {
-		//.... working much more
-	}
-
-	public void eat() {
-		//.... eating in launch break
-	}
-}
